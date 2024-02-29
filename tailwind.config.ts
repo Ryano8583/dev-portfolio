a36ee76b81ec11e59f45d6d0ab9ba2b559d59070
Ryano8583 <ryano8583@gmail.com>
2024-02-28 20:56:23 -0500

    BaseToggle.vue: created component file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,17 @@
 module.exports = {
     content: [
       './index.html',
-          './src/**/*.vue'
-      ],
+      './src/**/*.vue'
+    ],
+    darkMode: 'class',
     theme: {
-      extend: {},
+      extend: {
+        boxShadow: {
+          inner: 'inset 1px 1px 3px 0 #0008',
+          DEFAULT: '1px 1px 3px 0 #000a',
+          md: '2px 2px 6px 1px #000a'
+        },
+      },
     },
     plugins: [],
   }
