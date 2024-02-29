5a22ac1d99fa45cb1c580328e3c75f6a23788b3a
Ryano8583 <ryano8583@gmail.com>
2024-02-28 20:56:49 -0500

    TheThemeToggle.vue: created component file

---
+++ src/components/TheThemeToggle.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useDarkMode, isDarkMode } from "../composables/darkMode";
+
+import BaseToggle from "./BaseToggle.vue";
+
+useDarkMode({ from: "class" });
+</script>
+
+<template>
+  <BaseToggle id="dark-mode" v-model="isDarkMode" hide-label>
+    Dark mode
+  </BaseToggle>
+</template>
