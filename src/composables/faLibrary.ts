a9ce6df7a9223f53922c057209a7343cf40cefbf
Ryano8583 <ryano8583@gmail.com>
2024-03-01 21:49:38 -0500

    installed Font Awesome

---
+++ src/composables/faLibrary.ts
@@ -0,0 +1,8 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {} from "@fortawesome/free-regular-svg-icons";
+import {} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add();
+
+export { library, FontAwesomeIcon };
