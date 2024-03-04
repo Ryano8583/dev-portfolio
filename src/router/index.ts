b8c0b6a227cb6c1f578db6fd44d9b33453f01fef
Ryano8583 <ryano8583@gmail.com>
2024-03-03 21:08:11 -0500

    PortfolioView.vue: created & imported file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 const ResumeView = () => import("../views/ResumeView.vue");
+const PortfolioView = () => import("../views/PortfolioView.vue");
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: "/",
       name: "resume",
       component: ResumeView,
+    },
+    {
+      path: "/portfolio",
+      name: "portfolio",
+      component: PortfolioView,
     },
   ],
 });
