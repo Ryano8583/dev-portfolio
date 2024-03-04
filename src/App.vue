37d3d42bb46a5eb52acd738347e1fdce21027170
Ryano8583 <ryano8583@gmail.com>
2024-03-03 21:26:32 -0500

    TheHeader.vue: created & implemented file

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 
+import TheHeader from "./components/TheHeader.vue";
 import TheThemeToggle from "./components/TheThemeToggle.vue";
 </script>
 
 <template>
+  <TheHeader>Ryan Overmyer's Resume & Portfolio</TheHeader>
+
   <TheThemeToggle />
 
   <RouterView />
