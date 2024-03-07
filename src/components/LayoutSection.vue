3f2a47540aea7790b714ec8f91695a4a6b26308e
Ryano8583 <ryano8583@gmail.com>
2024-03-06 20:36:27 -0500

    LayoutSection.vue: created file

---
+++ src/components/LayoutSection.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+defineProps<{
+  /** Value of the text for the <h2>. */
+  heading: string;
+
+  /** Value of the [id] for this <section>. */
+  id: string;
+
+  /** Blue background */
+  useBlue?: boolean;
+
+  /** Green background */
+  useGreen?: boolean;
+
+  /** Orange background */
+  useOrange?: boolean;
+
+  /** Pink background */
+  usePink?: boolean;
+
+  /** Purple background */
+  usePurple?: boolean;
+}>();
+</script>
+
+<template>
+  <section
+    :id="id"
+    class="p-3 rounded-2xl border-[1px] border-transparent shadow dark:border-white dark:shadow-none sm:p-5"
+    :class="
+      useBlue
+        ? 'bg-blue-400 dark:bg-blue-999'
+        : useGreen
+          ? 'bg-green-400 dark:bg-green-999'
+          : useOrange
+            ? 'bg-orange-400 dark:bg-orange-999'
+            : usePink
+              ? 'bg-pink-400 dark:bg-pink-999'
+              : usePurple
+                ? 'bg-purple-400 dark:bg-purple-999'
+                : 'bg-zinc-400 dark:bg-zinc-800'
+    "
+  >
+    <h2
+      class="opacity-0 mb-3 text-5xl font-bold text-zinc-900 dark:text-zinc-50 translate-y-48 transition-fade duration-200 sm:mb-5"
+    >
+      {{ heading }}
+    </h2>
+    <slot />
+  </section>
+</template>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,23 @@ module.exports = {
           DEFAULT: '1px 1px 3px 0 #000a',
           md: '2px 2px 6px 1px #000a'
         },
+        colors: {
+          blue: {
+            999: '#0F204D'
+          },
+          green: {
+            999: '#0F3E22'
+          },
+          orange: {
+            999: '#652610'
+          },
+          pink: {
+            999: '#570F2C'
+          },
+          purple: {
+            999: '#300E48'
+          }
+        },
         transitionProperty: {
           fade: 'opacity, visibility, background-color, color, transform'
         }
