d2094b4f35efa293a72230100df876824598db50
Ryano8583 <ryano8583@gmail.com>
2024-03-13 10:35:54 -0400

    ResumeView.vue: import LayoutColumn&Section components

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,7 +1,22 @@
 <script setup lang="ts">
+import LayoutColumn from "@/components/LayoutColumn.vue";
+import LayoutSection from "@/components/LayoutSection.vue";
+
 import TheMain from "@/components/TheMain.vue";
 </script>
 
 <template>
-  <TheMain></TheMain>
+  <TheMain>
+    <LayoutSection id="section-1" heading="Contact" use-blue> </LayoutSection>
+
+    <LayoutSection id="section-2" heading="Education" use-green>
+    </LayoutSection>
+
+    <LayoutSection id="section-3" heading="Work Experience" use-purple>
+      <LayoutColumn> </LayoutColumn>
+    </LayoutSection>
+
+    <LayoutSection id="section-4" heading="Accomplishments" use-orange>
+    </LayoutSection>
+  </TheMain>
 </template>
