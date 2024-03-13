23c1ac287f0b0591750cf1c1e5cde21941aabf3f
Ryano8583 <ryano8583@gmail.com>
2024-03-13 10:11:38 -0400

    TheTopBar.vue: created & implemented file

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,20 @@ import BaseNavLink from "./components/BaseNavLink.vue";
 import TheFooter from "./components/TheFooter.vue";
 import TheHeader from "./components/TheHeader.vue";
 import TheThemeToggle from "./components/TheThemeToggle.vue";
+import TheTopBar from "./components/TheTopBar.vue";
 </script>
 
 <template>
-  <TheHeader>Ryan Overmyer's Resume & Portfolio</TheHeader>
+  <TheTopBar>
+    <TheHeader>Ryan Overmyer's Resume & Portfolio</TheHeader>
 
-  <BaseNavigation>
-    <BaseNavLink to="resume">Resume</BaseNavLink>
-    <BaseNavLink to="portfolio">Portfolio</BaseNavLink>
-  </BaseNavigation>
-  <TheThemeToggle />
+    <BaseNavigation>
+      <BaseNavLink to="resume">Resume</BaseNavLink>
+      <BaseNavLink to="portfolio">Portfolio</BaseNavLink>
+    </BaseNavigation>
+
+    <TheThemeToggle />
+  </TheTopBar>
 
   <router-view />
 
