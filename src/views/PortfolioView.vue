1764a8c4c90fd24aebce7b5284e8f19b373e6847
Ryano8583 <ryano8583@gmail.com>
2024-03-20 21:50:16 -0400

    PortfolioView.vue: prepped the page

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,7 +1,29 @@
 <script setup lang="ts">
+import { onBeforeUnmount, onMounted } from "vue";
+import { observerHeadings } from "@/composables/intersectionObserver";
+
+import LayoutContainer from "@/components/LayoutContainer.vue";
+import LayoutSection from "@/components/LayoutSection.vue";
+
 import TheMain from "@/components/TheMain.vue";
+
+onMounted(timeToObserve);
+onBeforeUnmount(() => {
+  observerHeadings.disconnect();
+});
+
+function timeToObserve() {
+  for (const target of document.querySelectorAll("section")) {
+    observerHeadings.observe(target);
+  }
+}
 </script>
 
 <template>
-  <TheMain></TheMain>
+  <TheMain>
+    <LayoutSection id="section-5" heading="">
+      <LayoutContainer>
+      </LayoutContainer>
+    </LayoutSection>
+  </TheMain>
 </template>
