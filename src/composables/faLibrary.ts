8ffadafe23d7c78bf40e3ab8da98ce71d62e4091
Ryano8583 <ryano8583@gmail.com>
2024-03-26 17:38:42 -0400

    BaseCalloutBox.vue: created file

--- src/composables/faLibrary.ts
+++ src/composables/faLibrary.ts
@@ -1,8 +1,12 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faMoon, faSun } from "@fortawesome/free-regular-svg-icons";
-import { faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCheck,
+  faCircleInfo,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faCheck, faMoon, faSun, faXmark);
+library.add(faCheck, faCircleInfo, faMoon, faSun, faXmark);
 
 export { library, FontAwesomeIcon };
