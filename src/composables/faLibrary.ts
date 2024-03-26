da0c0904840883d11252bc0a5b4a105992b0c01e
Ryano8583 <ryano8583@gmail.com>
2024-03-26 18:29:05 -0400

    BaseInlineQuestion.vue: created file

--- src/composables/faLibrary.ts
+++ src/composables/faLibrary.ts
@@ -3,10 +3,11 @@ import { faMoon, faSun } from "@fortawesome/free-regular-svg-icons";
 import {
   faCheck,
   faCircleInfo,
+  faCircleQuestion,
   faXmark,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faCheck, faCircleInfo, faMoon, faSun, faXmark);
+library.add(faCheck, faCircleInfo, faCircleQuestion, faMoon, faSun, faXmark);
 
 export { library, FontAwesomeIcon };
