3b55d1cd32bf060107c5760cec612c01db581923
Ryano8583 <ryano8583@gmail.com>
2024-03-28 15:51:49 -0400

    LayoutCollapsible.vue: modified colors for when it is expanded

--- src/portfolio/components/LayoutCollapsible.vue
+++ src/portfolio/components/LayoutCollapsible.vue
@@ -17,11 +17,11 @@ function toggle() {
 <template>
   <div class="p-4">
     <div
-      class="mx-auto p-3 border-[1.5px] rounded-md border-zinc-900 font-bold cursor-pointer motion-safe:duration-400 hover:bg-gray-200 focus-visible:bg-gray-400 dark:border-white dark:hover:bg-zinc-500 dark:focus-visible:bg-zinc-500"
+      class="mx-auto p-3 border-[1.5px] rounded-md border-zinc-900 font-bold text-zinc-900 cursor-pointer motion-safe:duration-400 focus-visible:outline-offset-2 dark:border-white"
       :class="
         isExpanded
-          ? 'rounded-b-none bg-blue-800 text-gray-50 hover:text-zinc-900 focus-visible:text-zinc-900 dark:bg-blue-200 dark:text-gray-900 dark:hover:text-zinc-50 dark:focus-visible:text-zinc-50'
-          : 'text-zinc-900 bg-gray-100 dark:bg-zinc-600 dark:text-zinc-50'
+          ? 'rounded-b-none bg-blue-200 hover:bg-blue-100 focus-visible:bg-blue-100'
+          : 'bg-gray-100 hover:bg-gray-200 focus-visible:bg-gray-400 dark:bg-zinc-600 dark:text-zinc-50 dark:hover:bg-zinc-500 dark:focus-visible:bg-zinc-500'
       "
       role="button"
       tabindex="0"
