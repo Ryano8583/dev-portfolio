57a5e9787956c178c69550bc459163b7d5d0f4f6
Ryano8583 <ryano8583@gmail.com>
2024-04-08 10:04:38 -0400

    BaseNavLink.vue: added spacebar support

--- src/components/BaseNavLink.vue
+++ src/components/BaseNavLink.vue
@@ -3,6 +3,10 @@ const props = defineProps<{
   /** Value for "name" property of route link */
   to: string;
 }>();
+
+function select(event: KeyboardEvent | any) {
+  event.target?.click();
+}
 </script>
 
 <template>
@@ -11,6 +15,7 @@ const props = defineProps<{
       :to="{ name: props.to }"
       class="p-2 rounded-md motion-safe:transition-colors motion-safe:duration-400 hover:bg-gray-200 hover:shadow-full focus-visible:outline-offset-4 dark:hover:bg-zinc-500 dark:hover:shadow-none"
       activeClass="bg-blue-800 shadow-full text-gray-50 hover:text-zinc-900 dark:bg-blue-200 dark:shadow-none dark:text-gray-900 dark:hover:text-zinc-50"
+      @keydown.space.prevent="select"
     >
       <slot />
     </RouterLink>
