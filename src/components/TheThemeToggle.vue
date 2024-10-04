8fa8ca1e6792f79d389a822acb0d1b2215f77a10
Ryan Overmyer <ryano8583@gmail.com>
2024-10-03 22:01:47 -0400

    TheThemeToggle.vue: translated text content

--- src/components/TheThemeToggle.vue
+++ src/components/TheThemeToggle.vue
@@ -1,17 +1,33 @@
 <script setup lang="ts">
+import { useI18n } from "vue-i18n";
+
 import { useDarkMode, isDarkMode } from "../composables/darkMode";
 import { FontAwesomeIcon } from "../composables/faLibrary";
 
 import BaseToggle from "./BaseToggle.vue";
+
+const { t } = useI18n({
+  useScope: "local",
+});
 
 useDarkMode({ from: "class" });
 </script>
 
 <template>
   <BaseToggle id="dark-mode" v-model="isDarkMode">
-    Dark mode
+    {{ t("") }}
     <template #icon>
       <FontAwesomeIcon :icon="['far', isDarkMode ? 'moon' : 'sun']" />
     </template>
   </BaseToggle>
 </template>
+
+<i18n lang="yaml">
+{
+  en: "Dark mode",
+  es: "Modo oscuro",
+  fr: "Mode sombre",
+  hi: "डार्क मोड",
+  zh: "暗黑模式",
+}
+</i18n>
